0343ef6c46a2706103fe83c0207b392b7c4f3797
Eugene Feinberg <eugene@recogni.com>
2020-10-12 13:25:00 -0700

    asic flow oddr

--- rtl/oddr.v
+++ rtl/oddr.v
@@ -24,75 +24,67 @@ THE SOFTWARE.
 
 // Language: Verilog 2001
 
-`timescale 1ns / 1ps
-
 /*
  * Generic ODDR module
  */
-module oddr #
-(
-    // target ("SIM", "GENERIC", "XILINX", "ALTERA")
-    parameter TARGET = "GENERIC",
-    // IODDR style ("IODDR", "IODDR2")
-    // Use IODDR for Virtex-4, Virtex-5, Virtex-6, 7 Series, Ultrascale
-    // Use IODDR2 for Spartan-6
-    parameter IODDR_STYLE = "IODDR2",
-    // Width of register in bits
-    parameter WIDTH = 1
-)
-(
-    input  wire             clk,
+ module oddr #
+    (
+        // target ("SIM", "GENERIC", "XILINX", "ALTERA")
+        parameter TARGET = "GENERIC",
+        // IODDR style ("IODDR", "IODDR2")
+        // Use IODDR for Virtex-4, Virtex-5, Virtex-6, 7 Series, Ultrascale
+        // Use IODDR2 for Spartan-6
+        parameter IODDR_STYLE = "IODDR2",
+        // Width of register in bits
+        parameter WIDTH = 1
+    )
+    (
+        input  wire             clk,
 
-    input  wire [WIDTH-1:0] d1,
-    input  wire [WIDTH-1:0] d2,
+        input  wire [WIDTH-1:0] d1,
+        input  wire [WIDTH-1:0] d2,
 
-    output wire [WIDTH-1:0] q
-);
+        output wire [WIDTH-1:0] q
+    );
 
-/*
+    /*
 
-Provides a consistent output DDR flip flop across multiple FPGA families
-              _____       _____       _____       _____
-    clk  ____/     \_____/     \_____/     \_____/     \_____
-         _ ___________ ___________ ___________ ___________ __
-    d1   _X____D0_____X____D2_____X____D4_____X____D6_____X__
-         _ ___________ ___________ ___________ ___________ __
-    d2   _X____D1_____X____D3_____X____D5_____X____D7_____X__
-         _____ _____ _____ _____ _____ _____ _____ _____ ____
-    d    _____X_D0__X_D1__X_D2__X_D3__X_D4__X_D5__X_D6__X_D7_
+    Provides a consistent output DDR flip flop across multiple FPGA families
+                  _____       _____       _____       _____
+        clk  ____/     \_____/     \_____/     \_____/     \_____
+             _ ___________ ___________ ___________ ___________ __
+        d1   _X____D0_____X____D2_____X____D4_____X____D6_____X__
+             _ ___________ ___________ ___________ ___________ __
+        d2   _X____D1_____X____D3_____X____D5_____X____D7_____X__
+             _____ _____ _____ _____ _____ _____ _____ _____ ____
+        d    _____X_D0__X_D1__X_D2__X_D3__X_D4__X_D5__X_D6__X_D7_
 
-*/
+    */
 
-genvar n;
-
-generate
-
-if (TARGET == "XILINX") begin
-  
-end else if (TARGET == "ALTERA") begin
-
-end else begin
     reg [WIDTH-1:0] d_reg_1 = {WIDTH{1'b0}};
     reg [WIDTH-1:0] d_reg_2 = {WIDTH{1'b0}};
-
-    reg [WIDTH-1:0] q_reg = {WIDTH{1'b0}};
+    reg [WIDTH-1:0] q_reg0 = {WIDTH{1'b0}};
+    reg [WIDTH-1:0] q_reg1 = {WIDTH{1'b0}};
 
     always @(posedge clk) begin
         d_reg_1 <= d1;
         d_reg_2 <= d2;
     end
-
     always @(posedge clk) begin
-        q_reg <= d1;
+        q_reg0 <= d1;
     end
 
-    always @(negedge clk) begin
-        q_reg <= d_reg_2;
+    reg clk_inv;
+    pd_ckinv
+     ddr_clk_inv
+     (
+    .clk_in            (clk),
+    .clk_inv_out       (clk_inv));
+
+    always @(posedge clk_inv) begin
+        q_reg1 <= d_reg_2;
     end
-
-    assign q = q_reg;
-end
-
-endgenerate
+    assign q = clk ? q_reg0 : q_reg1;
 
 endmodule
+
